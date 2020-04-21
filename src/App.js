b4c1677c6dee1ed30b18721f7e038834a64fb95a
Subin-Panta <subinpanta9@gmail.com>
2020-04-21 11:25:58 +0545

    basic framework built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="outerWrap">
+      <div className="App">
+        <div className="navBar">
+           <div className="logo">
+             <img src={logo}/>
+            </div>
+            <ul>
+              <li>Home</li>
+              <li>Search</li>
+              <li>Your Library</li>
+            </ul> 
+        </div>   
+      </div>
+      <div className="musicControls">
+        music musicControls
+      </div>
     </div>
   );
 }
